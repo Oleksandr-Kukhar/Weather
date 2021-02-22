f9801b977ed9baa95ec4cdfaa6f107cd5341fbc2
Oleksandr-Kukhar <vikakukhar@gmail.com>
2021-02-22 13:54:08 +0200

    Added logger

---
+++ Weather.Log/Domain/OperationResultState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.Logger.Domain
+{
+    public enum OperationResultState
+    {
+        Unknown,
+        Succeed,
+        Fail,
+        Error,
+        Warning,
+    }
+}
---
+++ Weather.Log/OperationResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Weather.Log.Domain;
+
+namespace Weather.Logger
+{
+    public class OperationResult
+    {
+        public string Message { get; protected set; }
+        public OperationResultState ResultState { get; protected set; }
+
+        public OperationResult()
+        {
+            Message = string.Empty;
+            ResultState = OperationResultState.Unknown;
+        }
+
+        public OperationResult(string message, OperationResultState resultState)
+        {
+            Message = message;
+            ResultState = resultState;
+        }
+
+        public static OperationResult GeOperationtResult(OperationResultState resultState, string message = "")
+        {
+            return new OperationResult(message, resultState);
+        }
+    }
+}
